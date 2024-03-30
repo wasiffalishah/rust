f2529be5afb498192b57c51a698ad37e4173cc3a
WASIFF ALI SHAH <137802448+wasiffalishah@users.noreply.github.com>
2024-03-30 12:09:05 +0500

    Create variables.rs

---
+++ variables.rs
@@ -0,0 +1,10 @@
+fn main(){
+    let w : u8 =9;                  // unsigned integer
+    let x : i8 = -7;                // signed integer
+    let is_true : bool = true;      // boolean
+    let c = "a";                    // char
+    let name = "Wasif" ;            // string
+    let z : f32 = 1.2 ;             // float
+    
+    println!("Unsigned Integer:{}, Signed Integer:{}, Boolean:{}, Character:{}, String:{}, Float: {}",w,x,is_true, c, name,z)
+}
