49abfee8092031821462a126e267c0e98988fcbb
WASIFF ALI SHAH <137802448+wasiffalishah@users.noreply.github.com>
2024-03-30 12:47:56 +0500

    Create mutibility.rs

---
+++ mutibility.rs
@@ -0,0 +1,9 @@
+fn main(){
+    let mut x = 4;
+    x = 6;
+    
+    println!("{}",x);
+    
+    //the vairable are immutable in rust, you cannot change it directly, if you want to chnage them then youu have to use keyword `mut` before the variabel name, in our chase let mut x = 4
+}
+
