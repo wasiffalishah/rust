a9b511e443a5d70dd215987132ce470fc0f34e11
WASIFF ALI SHAH <137802448+wasiffalishah@users.noreply.github.com>
2024-04-06 10:28:10 +0500

    Create vector.rs

---
+++ vector.rs
@@ -0,0 +1,11 @@
+
+fn main() {
+
+    let mut vec: Vec<i64> = vec![1,2,3,4,5];
+    println!("{:?}",vec.len()); 
+    vec[0];
+    vec.push(6);
+    vec.remove(0);
+    println!("{:?}", vec);
+    println!("{:?}",vec.len());
+}
